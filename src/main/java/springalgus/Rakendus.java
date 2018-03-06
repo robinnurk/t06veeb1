b72c84c0573027aa5d64b93da64190899ac0cd91
Robin Nurk <robin.nurk@gmail.com>
2018-03-06 09:14:10 +0200

    korrutus

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -2,7 +2,7 @@ package springalgus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
@@ -11,12 +11,17 @@ public class Rakendus {
     String tervitusfunktsioon() {
         return "Ahoi!";
     }
-    
-    @RequestMapping("/tervitus")
+
+  @RequestMapping("/tervitus")
     String tervitus2(String eesnimi){
         return "Tere, "+eesnimi;
     }
- 
+
+ 	@RequestMapping("/korrutus")
+	int korrutamine(int arv1, int arv2){
+		return arv1*arv2;
+	}
+
     public static void main(String[] args) {
 		//System.getProperties().put("server.port", 40305);
         SpringApplication.run(Rakendus.class, args);
