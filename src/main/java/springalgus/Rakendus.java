ff48e86ccfbd06b63a88582ff74a7b492bbf4cc6
Robin Nurk <robin.nurk@gmail.com>
2018-03-06 11:15:50 +0200

    uuendused

--- src/main/java/springalgus/Rakendus.java
+++ src/main/java/springalgus/Rakendus.java
@@ -3,8 +3,8 @@ package springalgus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.annotation.PostMapping;
-import org.springframework.web.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
@@ -20,18 +20,13 @@ public class Rakendus {
     }
 		//localhost:5556/tervitus?eesnimi=robin
 
-	@PostMapping("/korrutus/(arv1)/(arv2)")
+	@RequestMapping("/korrutus/(arv1)/(arv2)")
 	String korrutamine(@PathVariable String arv1, @PathVariable String arv2){
 		if(arv1==null){return "esimene arv puudub";}
 		int vastus=Integer.parseInt(arv1)*Integer.parseInt(arv2);
 		return String.valueOf(vastus);
 	}
 
-
- 	@RequestMapping("/korrutus")
-		int korrutamine(int arv1, int arv2){
-		return arv1*arv2;
-	}
 	//localhost:5556/korrutus?arv1=4&arv2=7
 
     public static void main(String[] args) {
